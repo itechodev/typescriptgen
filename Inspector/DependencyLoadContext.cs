1b3bec91c0612971b5ba59f8a54c7dfdb3d02a48
Willem Bijker <willem@itecho.co.za>
2023-07-17 09:24:33 +0200

    use assembly args. cleanup

--- Inspector/DependencyLoadContext.cs
+++ Inspector/DependencyLoadContext.cs
@@ -1,7 +1,9 @@
 using System.Reflection;
 using System.Runtime.Loader;
 
-class DependencyLoadContext : AssemblyLoadContext
+namespace Inspector;
+
+internal class DependencyLoadContext : AssemblyLoadContext
 {
     private readonly AssemblyDependencyResolver _resolver;
 
@@ -12,24 +14,13 @@ class DependencyLoadContext : AssemblyLoadContext
 
     protected override Assembly? Load(AssemblyName assemblyName)
     {
-        
         var assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
-        if (assemblyPath != null)
-        {
-            return LoadFromAssemblyPath(assemblyPath);
-        }
-
-        return null;
+        return assemblyPath != null ? LoadFromAssemblyPath(assemblyPath) : null;
     }
 
     protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
     {
         var libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
-        if (libraryPath != null)
-        {
-            return LoadUnmanagedDllFromPath(libraryPath);
-        }
-
-        return IntPtr.Zero;
+        return libraryPath != null ? LoadUnmanagedDllFromPath(libraryPath) : IntPtr.Zero;
     }
 }
--- Inspector/Program.cs
+++ Inspector/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
+using Inspector;
 
 namespace inspector;
 
 // dotnet pack
-//  dotnet tool install --global --add-source ./nupkg inspector  
+// dotnet tool install --global --add-source ./nupkg inspector  
 
-internal class Program
+public static class Program
 {
     static void Main(string[] args)
     {
@@ -23,29 +24,13 @@ internal class Program
             return;
         }
 
-        Console.WriteLine("Nice");
+        var assemblyPath = args[0];
+        Console.WriteLine("Getting assembly: " + assemblyPath);
+        // Loading assembly dependencies from a complete output path
+        // using a custom AssemblyLoadContext and AssemblyDependencyResolver
+        var loadContext = new DependencyLoadContext(assemblyPath);
+        var assembly = loadContext.LoadFromAssemblyPath(assemblyPath);
+
+        Console.WriteLine(string.Join("\n", assembly.GetExportedTypes().Select(t => t.Name)));
     }
 }
-//
-// // Loading assemblies and dependencies manually can be quite complex having only the .csproj file as a reference.
-// // As an simple alternative, choose the single output folder where all dependencies are copied to load the assembly from there. 
-// // Assembly.LoadFile resolves dependencies automatically by probing the Global Assembly Cache (GAC) and the folder from where the assembly was loaded for the necessary dependencies.
-//
-// var assemblyPath = "/Users/willem/projects/itest-leaf/Web/bin/Debug/net6.0/Web.dll";
-//
-// var loadContext = new DependencyLoadContext(assemblyPath);
-// var assembly = loadContext.LoadFromAssemblyName(new AssemblyName("Web"));
-//
-// // var assembly = Assembly.LoadFile(assemblyPath);
-//
-// foreach (var type in assembly.GetExportedTypes())
-// {
-//     if (type.Name == "AuthController")
-//     {
-//         
-//         type.GetMethods().Select(m => m.Name).ToList().ForEach(Console.WriteLine);
-//     }
-//     // Console.WriteLine($"{type.Name} {type.FullName}");
-// }
-//
-// // var types = assembly.GetTypes().Select(t => t.Name).ToList();
