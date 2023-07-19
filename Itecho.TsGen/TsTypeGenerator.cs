d71c4fc917edf39b405a332e2c1efe0a8ee78083
Willem Bijker <willem@itecho.co.za>
2023-07-19 10:06:02 +0200

    create TS type generator

---
+++ Itecho.TsGen/TsTypeGenerator.cs
@@ -0,0 +1,45 @@
+using Itecho.TsGen.TsTypes;
+
+namespace Itecho.TsGen;
+
+public class TsTypeGenerator
+{
+
+    public string Generate(TsType type)
+    {
+        return type switch
+        {
+            TsArray array => $"{Generate(array.ElementType)}[]",
+            TsBuildInType => "File",
+            TsIntersection intersection => string.Join(" & ", intersection.Types.Select(Generate)),
+            TsTuple tuple => $"[{string.Join(", ", tuple.Types.Select(Generate))}]",
+            TsUnion union => string.Join(" | ", union.Types.Select(Generate)),
+            TsDictionary dictionary =>
+                $"Record<{Generate(dictionary.Key)}, {Generate(dictionary.Value)}>",
+            TsEnum @enum => @enum.Name,
+            TsGeneric generic => generic.Name,
+            TsGenericReference genericReference =>
+                $"{genericReference.ReferencedType.Name}<{string.Join(", ", genericReference.Parameters.Select(Generate))}>",
+            TsInterface @interface => @interface.Name,
+            TsPrimitive primitive => GeneratePrimitive(primitive),
+            TsVoid @void => "void",
+            _ => throw new ArgumentOutOfRangeException(nameof(type))
+        };
+    }
+
+    private static string GeneratePrimitive(TsPrimitive primitive)
+    {
+        return primitive.Type switch
+        {
+            TsPrimitive.TsPrimitiveType.Undefined => "undefined",
+            TsPrimitive.TsPrimitiveType.Null => "null",
+            TsPrimitive.TsPrimitiveType.String => "string",
+            TsPrimitive.TsPrimitiveType.Number => "number",
+            TsPrimitive.TsPrimitiveType.Boolean => "boolean",
+            TsPrimitive.TsPrimitiveType.Date => "Date",
+            TsPrimitive.TsPrimitiveType.Unknown => "unknown",
+            TsPrimitive.TsPrimitiveType.Any => "any",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
