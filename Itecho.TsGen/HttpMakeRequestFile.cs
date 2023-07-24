2cd3a26cbd13c1175220a66c6e50225fac1c3d36
Willem Bijker <willem@itecho.co.za>
2023-07-24 11:28:47 +0200

    rename http options to  request options

--- Itecho.TsGen/HttpMakeRequestFile.cs
+++ Itecho.TsGen/HttpMakeRequestFile.cs
@@ -8,10 +8,10 @@ public static class HttpMakeRequestFile
     {
         var file = new TsFile();
 
-        file.Add(TsExp.Literal(@"import {type HttpOptions} from './httpOptions';
+        file.Add(TsExp.Literal(@"import {type RequestOptions} from './requestOptions';
 
 //  (url: string, options?: HttpOptions) => Promise<T>;
-export default function makeRequest<T>(url: string, options?: HttpOptions): Promise<T> {
+export default function makeRequest<T>(url: string, options?: RequestOptions): Promise<T> {
     throw new Error('Function not implemented.');
 }"));
         return file;
